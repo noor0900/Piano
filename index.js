a14b57d315a06ef131cb9fe3ea7c5e65c7c8a9ef
noor0900 <noorfatima6227@gmail.com>
2024-09-12 22:21:11 +0500

    add

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ window.addEventListener('keypress', (e) => {
         btn = document.querySelector(`button[data-key="${keycode}"]`);
 
     }
-    if (keycode >= 97 && keycode <= 121) {
-        btn = document.querySelector(`button[data-key="${keycode}"]`);
-
-    }
     if (audio) {
         audio.play();
         btn.classList.add("press");
