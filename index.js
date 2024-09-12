91fd948f39adbeec46d08b11cfebfcde09d10201
noor0900 <noorfatima6227@gmail.com>
2024-09-12 22:18:11 +0500

    add

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 window.addEventListener('keypress', (e) => {
-    const keycode = event.keyCode;
-    const keyCode = e.keyCode;
+    const keycode = e.keyCode;
 
-let btn = undefined;
-const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+    let btn = undefined;
+    const audio = document.querySelector(`audio[data-key="${keycode}"]`);
 
 
-if (keycode >= 97 && keycode <= 121) {
-    btn = document.querySelector(`button[data-key="${keyCode}"]`);
+    if (keycode >= 97 && keycode <= 121) {
+        btn = document.querySelector(`button[data-key="${keycode}"]`);
 
-}
-if (keycode >= 97 && keycode <= 121) {
-    btn = document.querySelector(`button[data-key="${keyCode}"]`);
+    }
+    if (keycode >= 97 && keycode <= 121) {
+        btn = document.querySelector(`button[data-key="${keycode}"]`);
 
-}
-if (audio) {
-    audio.play();
-    btn.classList.add("press");
-    setTimeout(() => {  
-        btn.classList.remove("press");
-    },600);
-}
+    }
+    if (audio) {
+        audio.play();
+        btn.classList.add("press");
+        setTimeout(() => {
+            btn.classList.remove("press");
+        }, 100);
+    }
 });
